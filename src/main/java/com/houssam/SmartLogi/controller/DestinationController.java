0eac35b67b1ee73d20abdcb11655c2e438655049
houssamlambara <lambarahoussam@gmail.com>
2025-10-27 20:37:16 +0100

    ajout du controller

---
+++ src/main/java/com/houssam/SmartLogi/controller/DestinationController.java
@@ -0,0 +1,9 @@
+package com.houssam.SmartLogi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/destinations")
+public class DestinationController {
+}
---
+++ src/main/java/com/houssam/SmartLogi/controller/LivreurController.java
@@ -0,0 +1,9 @@
+package com.houssam.SmartLogi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/livreurs")
+public class LivreurController {
+}
