85aec4f62d8b08f23d5539ba22f6b14aa4209f30
houssamlambara <lambarahoussam@gmail.com>
2025-10-28 12:21:08 +0100

    Implementation complete du service ClientExpediteur avec CRUD

--- src/main/java/com/houssam/SmartLogi/service/ClientExpediteurService.java
+++ src/main/java/com/houssam/SmartLogi/service/ClientExpediteurService.java
@@ -1,7 +1,20 @@
 package com.houssam.SmartLogi.service;
 
+import com.houssam.SmartLogi.mapper.ClientExpediteurMapper;
+import com.houssam.SmartLogi.repository.ClientExpediteurRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientExpediteurService {
+    private final ClientExpediteurRepository repository;
+    private final ClientExpediteurMapper mapper;
+
+    public ClientExpediteurService(clientExpediteurRepository repository, ClientExpediteurMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public ClientExpediteurMapper getMapper() {
+        return mapper;
+    }
 }
