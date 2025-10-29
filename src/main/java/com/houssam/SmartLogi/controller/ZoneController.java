756a6f23a885eb107b3443cf79851521554390f3
houssamlambara <lambarahoussam@gmail.com>
2025-10-29 20:00:53 +0100

    feat(zone): add Zone CRUD functionality

--- src/main/java/com/houssam/SmartLogi/controller/ZoneController.java
+++ src/main/java/com/houssam/SmartLogi/controller/ZoneController.java
@@ -1,9 +1,49 @@
 package com.houssam.SmartLogi.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.houssam.SmartLogi.dto.ZoneDTO;
+import com.houssam.SmartLogi.response.ApiResponse;
+import com.houssam.SmartLogi.service.ZoneService;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/zone")
 public class ZoneController {
+
+    public final ZoneService service;
+
+    public ZoneController(ZoneService service){
+        this.service = service;
+    }
+
+    @PostMapping
+    public ResponseEntity<ApiResponse<ZoneDTO>> createZone(@Valid @RequestBody ZoneDTO dto){
+        ZoneDTO created = service.createZone(dto);
+        return ResponseEntity.ok(new ApiResponse<>("Zone créée avec succès", created));
+    }
+
+    @GetMapping
+    public ResponseEntity<ApiResponse<List<ZoneDTO>>> getAllZones(){
+        List<ZoneDTO> zones = service.getAllZones();
+        return ResponseEntity.ok(new ApiResponse<>("Liste des zones", zones));
+    }
+
+    @GetMapping("{id}")
+        public ResponseEntity<ApiResponse<ZoneDTO>> getZoneById(@PathVariable Long id){
+            ZoneDTO zone = service.getZoneById(id);
+            if (zone == null){
+                return ResponseEntity.status(404).body(new ApiResponse<>("Zone non trouvée", null));
+            } else {
+                return ResponseEntity.ok(new ApiResponse<>("Zone trouvée", zone));
+            }
+        }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ApiResponse<Void>> deleteZone(@PathVariable Long id) {
+        service.deleteZone(id);
+        return ResponseEntity.ok(new ApiResponse<>("Zone supprimée avec succès", null));
+    }
 }
--- src/main/java/com/houssam/SmartLogi/service/ZoneService.java
+++ src/main/java/com/houssam/SmartLogi/service/ZoneService.java
@@ -1,7 +1,46 @@
 package com.houssam.SmartLogi.service;
 
+import com.houssam.SmartLogi.dto.ZoneDTO;
+import com.houssam.SmartLogi.mapper.ZoneMapper;
+import com.houssam.SmartLogi.model.Zone;
+import com.houssam.SmartLogi.repository.ZoneRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ZoneService {
+
+    private final ZoneRepository repository;
+    private final ZoneMapper mapper;
+
+    public ZoneService(ZoneRepository repository, ZoneMapper mapper) {
+        this.repository = repository;
+        this.mapper = mapper;
+    }
+
+    public ZoneDTO createZone(ZoneDTO dto) {
+        Zone entity = mapper.toEntity(dto);
+        Zone saved = repository.save(entity);
+        return mapper.toDTO(saved);
+    }
+
+    public List<ZoneDTO> getAllZones(){
+        return repository.findAll()
+                .stream()
+                .map(mapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public ZoneDTO getZoneById(Long id) {
+        return repository.findById(id)
+                .map(mapper::toDTO)
+                .orElse(null);
+    }
+
+    public void deleteZone(Long id){
+        repository.deleteById(id);
+    }
+
 }
