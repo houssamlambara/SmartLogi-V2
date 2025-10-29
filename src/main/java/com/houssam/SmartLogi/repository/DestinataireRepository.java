cafb9ca26cbfa896b7d606680546cec11009e6ec
houssamlambara <lambarahoussam@gmail.com>
2025-10-29 17:05:17 +0100

    refactor(repository): update repositories

--- src/main/java/com/houssam/SmartLogi/repository/DestinataireRepository.java
+++ src/main/java/com/houssam/SmartLogi/repository/DestinataireRepository.java
@@ -1,9 +1,10 @@
 package com.houssam.SmartLogi.repository;
 
 import com.houssam.SmartLogi.model.Colis;
+import com.houssam.SmartLogi.model.Destinataire;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface DestinataireRepository extends JpaRepository<Colis,String> {
+public interface DestinataireRepository extends JpaRepository<Destinataire, Long> {
 }
