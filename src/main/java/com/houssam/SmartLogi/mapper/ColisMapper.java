72095c44d84c43e45648dc45ffd95962540abec9
houssamlambara <lambarahoussam@gmail.com>
2025-10-30 10:23:15 +0100

    Mise a jour de ColisMapper

--- src/main/java/com/houssam/SmartLogi/mapper/ColisMapper.java
+++ src/main/java/com/houssam/SmartLogi/mapper/ColisMapper.java
@@ -8,11 +8,17 @@ import org.mapstruct.Mapping;
 @Mapper(componentModel = "spring")
 public interface ColisMapper {
 
+    @Mapping(source = "livreur.id", target = "livreurId")
+    @Mapping(source = "clientExpediteur.id", target = "clientExpediteurId")
+    @Mapping(source = "destinataire.id", target = "destinataireId")
+    @Mapping(source = "zone.id", target = "zoneId")
     ColisDTO toDTO(Colis colis);
 
     @Mapping(target = "livreur", ignore = true)
     @Mapping(target = "clientExpediteur", ignore = true)
     @Mapping(target = "destinataire", ignore = true)
     @Mapping(target = "zone", ignore = true)
+    @Mapping(target = "historiqueLivraisons", ignore = true)
+    @Mapping(target = "produits", ignore = true)
     Colis toEntity(ColisDTO dto);
 }
